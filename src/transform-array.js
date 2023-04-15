d5cd0dab1ff9d3126b6681280546407238c1d127
Andrei Paleshka <andrepoleshko@gmail.com>
2023-04-15 13:04:22 +0200

    feat: implement transform-array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,62 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if(!Array.isArray(arr)) {
+    throw new Error("\'arr\' parameter must be an instance of the Array!");
+  }
+  
+  let copyArr = [...arr];
+  const discardNext = '--discard-next';
+  const discardPrev = '--discard-prev';
+  const doubleNext = '--double-next';
+  const doublePrev = '--double-prev';
+  const getIdx = (transform) => {return copyArr.indexOf(transform)}
+  let idx;
+  
+  if (copyArr.includes(discardNext)) {
+    idx = getIdx(discardNext);
+    if (idx === copyArr.length - 1) {
+      copyArr.splice(idx, 1);
+    }
+    else {
+      copyArr.splice(idx, 2);
+      if (copyArr.includes(discardPrev)) {
+        copyArr.splice(idx, 1);
+      }
+      else if (copyArr.includes(doublePrev)) {
+        copyArr.splice(idx, 1);
+      }
+    }
+  }
+  if (copyArr.includes(doubleNext)) {
+    idx = getIdx(doubleNext);
+    if (idx === copyArr.length - 1) {
+      copyArr.splice(idx, 1);
+    }
+    else {
+      copyArr.fill(copyArr[idx + 1], idx, idx + 1);
+    }
+  }
+  if (copyArr.includes(discardPrev)) {
+    idx = getIdx(discardPrev);
+    if (idx === 0) {
+      copyArr.splice(idx, 1);
+    }
+    else {
+      copyArr.splice(idx - 1, 2);
+    }
+  }
+  if (copyArr.includes(doublePrev)) {
+    idx = getIdx(doublePrev);
+    if (idx === 0) {
+      copyArr.splice(idx, 1);
+    }
+    else {
+      copyArr.fill(copyArr[idx - 1], idx, idx + 1);
+    }
+  }  
+  return copyArr;
 }
 
 module.exports = {
